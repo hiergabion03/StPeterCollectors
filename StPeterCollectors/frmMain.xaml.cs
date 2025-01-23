14f6e90f95ead4c558f6f7753983fe246323781d
hiergabion03 <thirdgabion03@gmail.com>
2025-01-22 20:38:55 -0500

    Add project files.

---
+++ StPeterCollectors/frmMain.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace StPeterCollectors
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (rbtnMale.IsChecked == true)
+            {
+                MessageBox.Show($"Hello {rbtnMale.Content}");
+            }
+            else { 
+                MessageBox.Show( $" Hello {rbtnFemale.Content}" );
+            }
+        }
+        
+    }
+}
